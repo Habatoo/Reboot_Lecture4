44ac42152b6a2703df404a26ba80f3dac469825f
Liman26 <16686319@sigma.sbrf.ru>
2021-03-23 16:51:31 +0300

    Добавлено доп.задание

--- src/main/java/sbp/exceptions/WorkWithExceptions.java
+++ src/main/java/sbp/exceptions/WorkWithExceptions.java
@@ -16,6 +16,38 @@ public class WorkWithExceptions
          */
     }
 
+    /**
+     * (* - необязательно) Доп. задание.
+     * Выписать в javadoc (здесь) - все варианты оптимизации и устранения недочётов метода
+     * @throws IllegalStateException
+     * @throws Exception
+     * @throws RuntimeException
+     */
+    public void hardExceptionProcessing() throws IllegalStateException, Exception, RuntimeException
+    {
+        System.out.println("Start");
+        try
+        {
+            System.out.println("Step 1");
+            throw new IllegalArgumentException();
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Catch IllegalArgumentException");
+            throw new RuntimeException("Step 2");
+        }
+        catch (RuntimeException e)
+        {
+            System.out.println("Catch RuntimeException");
+            throw new RuntimeException("Step 3");
+        }
+        finally
+        {
+            System.out.println("Step finally");
+            throw new RuntimeException("From finally");
+        }
+    }
+
     private void throwCheckedException() throws Exception
     {
         throw new Exception("Checked exception");
