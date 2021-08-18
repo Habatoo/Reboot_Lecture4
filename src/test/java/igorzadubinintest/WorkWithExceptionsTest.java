065df565e12dcd181792c529a6fc61c6efdce225
Igor Zadubinin <igor.zadubinin@gmail.com>
2021-08-18 21:14:01 +0300

    Добавлена проверка выброса своего исключения

--- src/test/java/igorzadubinintest/WorkWithExceptionsTest.java
+++ src/test/java/igorzadubinintest/WorkWithExceptionsTest.java
@@ -2,6 +2,7 @@ package igorzadubinintest;
 
 import igorzadubinin.exceptions.MyException;
 import igorzadubinin.exceptions.WorkWithExceptions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,9 +21,13 @@ class WorkWithExceptionsTest {
 
     /**
      * Тестирование работы метода {@link WorkWithExceptions#exceptionProcessing()}
+     * Проверяется: метод должен выбросить исключение {@link MyException}
+     * Перед завершением работы метода выводится сообщение Finish.
      */
     @Test
     void exceptionProcessing_Test() throws MyException {
-        workWithExceptions.exceptionProcessing();
+        Assertions.assertThrows(MyException.class, () -> {
+            workWithExceptions.exceptionProcessing();
+        });
     }
 }
